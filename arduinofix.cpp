9b91aff4b816668f58b6b883fdbcf942d37d773b
hafizuuu <96855219+hafizuuu@users.noreply.github.com>
2025-01-09 07:32:13 +0700

    Update arduinofix.cpp

--- arduinofix.cpp
+++ arduinofix.cpp
@@ -1,60 +1,223 @@
-import cv2
-import pytesseract
-from pytesseract import Output
-import serial
-import time
+#include <Servo.h>
+#include <Wire.h>     
+#include <LiquidCrystal_I2C.h> 
 
-#komunikasi serial dengan Arduino
-arduino = serial.Serial('/dev/ttyUSB0', 9600) 
-time.sleep(2)
+Servo myServo; 
 
-#buka kamera
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+int red1 = 3;
+int green1 = 5;
+int red2 = 10;
+int green2 = 11;
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if not ret:
-        continue
+const int ir1 = 2;
+const int ir2 = 4;
+const int ir3 = 7;
 
-    #Tesseract untuk membaca teks
-    d = pytesseract.image_to_data(frame, output_type=Output.DICT, lang='eng')
-    n_boxes = len(d['text'])
+int posisiservo = 0;
 
-    for i in range(n_boxes):
-        if int(d['conf'][i]) > 60:  # Confidence threshold
-            (text, x, y, w, h) = (
-                d['text'][i],
-                d['left'][i],
-                d['top'][i],
-                d['width'][i],
-                d['height'][i],
-            )
-            # Jangan tampilkan teks kosong
-            if text and text.strip() != "":
-                frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                frame = cv2.putText(
-                    frame,
-                    text,
-                    (x, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5,
-                    (0, 255, 0),
-                    2,
-                )
+int objectir1 = 0;
+int objectir2 = 0;
+int objectir3 = 0;
 
-                if "D333A" in text:  
-                    arduino.write(b'1') 
-                    print("Plat nomor terdeteksi:", text)
-                else:
-                    arduino.write(b'0') 
-                    print("Plat nomor tidak terdeteksi")
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+void setup() {
+  Serial.begin(9600);  
+  myServo.attach(9);   
+  myServo.write(0);    
+  lcd.init();                  
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print(" SMART PARKING ");  
+  lcd.setCursor(0, 1);
+  lcd.print("   MANAGEMENT ");       
+}
 
-cap.release()
-cv2.destroyAllWindows()
-arduino.close()
+void loop() {
+  
+  objectir1 = digitalRead(ir1);
+  objectir2 = digitalRead(ir2);
+  objectir3 = digitalRead(ir3);
+
+  if(objectir3 == LOW){
+    if(posisiservo == 0){
+      for (posisiservo = 0; posisiservo <= 90; posisiservo += 1) { 
+      myServo.write(posisiservo);
+      delay(10); 
+      }
+    }
+    posisiservo = 90;  
+    Serial.println("Servo ke 90 derajat"); 
+  }else{
+    if(posisiservo == 90){
+      for (posisiservo = 90; posisiservo >= 0; posisiservo -= 1) { 
+      myServo.write(posisiservo);
+      delay(10); 
+      }
+    }
+    posisiservo = 0;
+    Serial.println("Servo ke 0 derajat");
+  }
+ 
+  if(objectir1 == HIGH && objectir2 == HIGH){
+    
+
+    lcd.setCursor(0, 0);
+    lcd.print(" SMART PARKING ");  
+    lcd.setCursor(0, 1);
+    lcd.print(" 2 EMPTY SLOT  ");
+    analogWrite(red1, 0);
+    analogWrite(green1, 255);
+    analogWrite(red2, 0);
+    analogWrite(green2, 255);
+    Serial.flush();
+    
+    if (Serial.available() > 0) {
+      char command = Serial.read(); 
+      
+      if (command == '1') {
+        if(posisiservo == 0){
+          for (posisiservo = 0; posisiservo <= 90; posisiservo += 1) { 
+          myServo.write(posisiservo);
+          delay(10); 
+          }
+        }
+        posisiservo = 90;  
+        Serial.println("Servo ke 90 derajat");
+        delay(5000);  
+      } 
+      else if (command == '0') {
+          
+        if(posisiservo == 90){
+          for (posisiservo = 90; posisiservo >= 0; posisiservo -= 1) { 
+          myServo.write(posisiservo);
+          delay(10); 
+          }
+        }
+        posisiservo = 0;
+        Serial.println("Servo ke 0 derajat");
+      }
+      
+    }
+  }
+  else if(objectir1 == LOW && objectir2 == HIGH){
+    
+
+    lcd.setCursor(0, 0);
+    lcd.print(" SMART PARKING ");  
+    lcd.setCursor(0, 1);
+    lcd.print(" 1 EMPTY SLOT  ");
+    analogWrite(red1, 255);
+    analogWrite(green1, 0);
+    analogWrite(red2, 0);
+    analogWrite(green2, 255);
+
+    if (Serial.available() > 0) {
+      char command = Serial.read(); 
+      
+      if (command == '1') {
+        if(posisiservo == 0){
+          for (posisiservo = 0; posisiservo <= 90; posisiservo += 1) { 
+          myServo.write(posisiservo);
+          delay(10); 
+          }
+        }
+        posisiservo = 90;  
+        Serial.println("Servo ke 90 derajat");
+        delay(5000);  
+      } 
+      else if (command == '0') {
+          
+        if(posisiservo == 90){
+          for (posisiservo = 90; posisiservo >= 0; posisiservo -= 1) { 
+          myServo.write(posisiservo);
+          delay(10); 
+          }
+        }
+        posisiservo = 0;
+        Serial.println("Servo ke 0 derajat");
+      }
+      
+
+    }
+  }
+  else if(objectir1 == HIGH && objectir2 == LOW){
+    
+
+    lcd.setCursor(0, 0);
+    lcd.print(" SMART PARKING ");  
+    lcd.setCursor(0, 1);
+    lcd.print(" 1 EMPTY SLOT  ");
+    analogWrite(red1, 0);
+    analogWrite(green1, 255);
+    analogWrite(red2, 255);
+    analogWrite(green2, 0);
+
+    if (Serial.available() > 0) {
+      char command = Serial.read(); 
+      
+      if (command == '1') {
+        if(posisiservo == 0){
+          for (posisiservo = 0; posisiservo <= 90; posisiservo += 1) { 
+          myServo.write(posisiservo);
+          delay(10); 
+          }
+        }
+        posisiservo = 90;  
+        Serial.println("Servo ke 90 derajat"); 
+        delay(5000); 
+      } 
+      else if (command == '0') {
+          
+        if(posisiservo == 90){
+          for (posisiservo = 90; posisiservo >= 0; posisiservo -= 1) { 
+          myServo.write(posisiservo);
+          delay(10); 
+          }
+        }
+        posisiservo = 0;
+        Serial.println("Servo ke 0 derajat");
+      }
+      
+
+    }
+  }
+  else{
+    Serial.flush();
+    lcd.setCursor(0, 0);
+    lcd.print(" SMART PARKING ");  
+    lcd.setCursor(0, 1);
+    lcd.print(" PARKING FULL  ");
+
+    analogWrite(red1, 255);
+    analogWrite(green1, 0);
+    analogWrite(red2, 255);
+    analogWrite(green2, 0);
+    if (Serial.available() > 0) {
+      char command = Serial.read(); 
+      
+      if (command == '1') {
+        if(posisiservo == 90){
+          for (posisiservo = 90; posisiservo >= 0; posisiservo -= 1) { 
+          myServo.write(posisiservo);
+          delay(10); 
+          }
+        }
+        posisiservo = 0;
+        Serial.println("Servo ke 0 derajat");  
+      } 
+      else if (command == '0') {
+          
+        if(posisiservo == 90){
+          for (posisiservo = 90; posisiservo >= 0; posisiservo -= 1) { 
+          myServo.write(posisiservo);
+          delay(10); 
+          }
+        }
+        posisiservo = 0;
+        Serial.println("Servo ke 0 derajat");
+      }
+    }  
+  }
+
+}
